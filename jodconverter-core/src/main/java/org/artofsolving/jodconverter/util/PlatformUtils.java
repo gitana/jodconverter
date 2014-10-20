6378c6f292779da6f1ea1065d10025515f7a4987
Fran Aquino <fj.aquino@gmail.com>
2014-10-20 18:10:41 +0200

    Start full code sweep so I can figure all this out

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/util/PlatformUtils.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/util/PlatformUtils.java
@@ -12,27 +12,22 @@
 //
 package org.artofsolving.jodconverter.util;
 
-public class PlatformUtils
-{
-    private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
+public class PlatformUtils {
+  private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
 
-    private PlatformUtils()
-    {
-        throw new AssertionError("utility class must not be instantiated");
-    }
+  private PlatformUtils() {
+    throw new AssertionError("utility class must not be instantiated");
+  }
 
-    public static boolean isLinux()
-    {
-        return OS_NAME.startsWith("linux");
-    }
+  public static boolean isLinux() {
+    return OS_NAME.startsWith("linux");
+  }
 
-    public static boolean isMac()
-    {
-        return OS_NAME.startsWith("mac");
-    }
+  public static boolean isMac() {
+    return OS_NAME.startsWith("mac");
+  }
 
-    public static boolean isWindows()
-    {
-        return OS_NAME.startsWith("windows");
-    }
+  public static boolean isWindows() {
+    return OS_NAME.startsWith("windows");
+  }
 }
