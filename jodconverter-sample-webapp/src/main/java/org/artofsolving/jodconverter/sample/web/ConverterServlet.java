854e50914bd7210c9c84f778a3fbced88c7dcaa6
Fran Aquino <fj.aquino@gmail.com>
2014-10-24 20:46:24 +0200

    Code reformat. Switched from JUL to slf4j + Logback for logging

--- jodconverter-sample-webapp/src/main/java/org/artofsolving/jodconverter/sample/web/ConverterServlet.java
+++ jodconverter-sample-webapp/src/main/java/org/artofsolving/jodconverter/sample/web/ConverterServlet.java
@@ -1,11 +1,14 @@
 package org.artofsolving.jodconverter.sample.web;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
+
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,88 +22,98 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.artofsolving.jodconverter.OfficeDocumentConverter;
 import org.artofsolving.jodconverter.document.DocumentFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ConverterServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -591469426224201748L;
+  private static final long   serialVersionUID = -591469426224201748L;
 
-    private final Logger logger = Logger.getLogger(getClass().getName());
+//  private final Logger      logger           = Logger.getLogger(getClass().getName());
+  private static final Logger LOG              = LoggerFactory.getLogger(ConverterServlet.class); 
+  
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    if (!ServletFileUpload.isMultipartContent(request)) {
+      response.sendError(HttpServletResponse.SC_FORBIDDEN, "only multipart requests are allowed");
+      return;
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (!ServletFileUpload.isMultipartContent(request)) {
-        	response.sendError(HttpServletResponse.SC_FORBIDDEN, "only multipart requests are allowed");
-        	return;
-        }
+    WebappContext webappContext = WebappContext.get(getServletContext());
+    ServletFileUpload fileUpload = webappContext.getFileUpload();
+    OfficeDocumentConverter converter = webappContext.getDocumentConverter();
 
-		WebappContext webappContext = WebappContext.get(getServletContext());
-		ServletFileUpload fileUpload = webappContext.getFileUpload();
-		OfficeDocumentConverter converter = webappContext.getDocumentConverter();
+    String outputExtension = FilenameUtils.getExtension(request.getRequestURI());
 
-		String outputExtension = FilenameUtils.getExtension(request.getRequestURI());
-		        
-		FileItem uploadedFile;
-		try {
-			uploadedFile = getUploadedFile(fileUpload, request);
-		} catch (FileUploadException fileUploadException) {
-		    throw new ServletException(fileUploadException);
-		}
-		if (uploadedFile == null) {
-			throw new NullPointerException("uploaded file is null");
-		}
-        String inputExtension = FilenameUtils.getExtension(uploadedFile.getName());
+    FileItem uploadedFile;
+    try {
+      uploadedFile = getUploadedFile(fileUpload, request);
+    } catch (FileUploadException fileUploadException) {
+      throw new ServletException(fileUploadException);
+    }
+    if (uploadedFile == null) {
+      throw new NullPointerException("uploaded file is null");
+    }
+    String inputExtension = FilenameUtils.getExtension(uploadedFile.getName());
 
-        String baseName = FilenameUtils.getBaseName(uploadedFile.getName());
-        File inputFile = File.createTempFile(baseName, "." + inputExtension);
-        writeUploadedFile(uploadedFile, inputFile);
-        File outputFile = File.createTempFile(baseName, "." + outputExtension);
-        try {
-            DocumentFormat outputFormat = converter.getFormatRegistry().getFormatByExtension(outputExtension);
-        	long startTime = System.currentTimeMillis();
-        	converter.convert(inputFile, outputFile);
-        	long conversionTime = System.currentTimeMillis() - startTime;
-        	logger.info(String.format("successful conversion: %s [%db] to %s in %dms", inputExtension, inputFile.length(), outputExtension, conversionTime));
-        	response.setContentType(outputFormat.getMediaType());
-            response.setHeader("Content-Disposition", "attachment; filename="+ baseName + "." + outputExtension);
-            sendFile(outputFile, response);
-        } catch (Exception exception) {
-            logger.severe(String.format("failed conversion: %s [%db] to %s; %s; input file: %s", inputExtension, inputFile.length(), outputExtension, exception, inputFile.getName()));
-        	throw new ServletException("conversion failed", exception);
-        } finally {
-        	outputFile.delete();
-        	inputFile.delete();
-        }
-	}
+    String baseName = FilenameUtils.getBaseName(uploadedFile.getName());
+    File inputFile = File.createTempFile(baseName, "." + inputExtension);
+    writeUploadedFile(uploadedFile, inputFile);
+    File outputFile = File.createTempFile(baseName, "." + outputExtension);
+    try {
+      DocumentFormat outputFormat = converter.getFormatRegistry().getFormatByExtension(outputExtension);
+      long startTime = System.currentTimeMillis();
+      converter.convert(inputFile, outputFile);
+      long conversionTime = System.currentTimeMillis() - startTime;
+//      logger.info(String.format("successful conversion: %s [%db] to %s in %dms", inputExtension, inputFile.length(),
+//          outputExtension, conversionTime));
+      LOG.info("successful conversion: {} [{} bytes] to {} in {} ms", 
+          inputExtension, inputFile.length(), outputExtension, conversionTime);
+      response.setContentType(outputFormat.getMediaType());
+      response.setHeader("Content-Disposition", "attachment; filename=" + baseName + "." + outputExtension);
+      sendFile(outputFile, response);
+    } catch (Exception exception) {
+//      logger.severe(String.format("failed conversion: %s [%db] to %s; %s; input file: %s", inputExtension,
+//          inputFile.length(), outputExtension, exception, inputFile.getName()));
+      LOG.error("failed conversion: {} [{} bytes] to {}; {}; input file: {}", 
+          inputExtension, inputFile.length(), outputExtension, exception, inputFile.getName());
+      throw new ServletException("conversion failed", exception);
+    } finally {
+      outputFile.delete();
+      inputFile.delete();
+    }
+  }
 
-	private void sendFile(File file, HttpServletResponse response) throws IOException {
-		response.setContentLength((int) file.length());
-        InputStream inputStream = null;
-        try {
-        	inputStream = new FileInputStream(file);
-            IOUtils.copy(inputStream, response.getOutputStream());
-        } finally {
-        	IOUtils.closeQuietly(inputStream);
-        }
-	}
+  private void sendFile(File file, HttpServletResponse response) throws IOException {
+    response.setContentLength((int) file.length());
+    InputStream inputStream = null;
+    try {
+      inputStream = new FileInputStream(file);
+      IOUtils.copy(inputStream, response.getOutputStream());
+    } finally {
+      IOUtils.closeQuietly(inputStream);
+    }
+  }
 
-	private void writeUploadedFile(FileItem uploadedFile, File destinationFile) throws ServletException {
-        try {
-			uploadedFile.write(destinationFile);
-		} catch (Exception exception) {
-			throw new ServletException("error writing uploaded file", exception);
-		}
-		uploadedFile.delete();
-	}
+  private void writeUploadedFile(FileItem uploadedFile, File destinationFile) throws ServletException {
+    try {
+      uploadedFile.write(destinationFile);
+    } catch (Exception exception) {
+      throw new ServletException("error writing uploaded file", exception);
+    }
+    uploadedFile.delete();
+  }
 
-	private FileItem getUploadedFile(ServletFileUpload fileUpload, HttpServletRequest request) throws FileUploadException {
-		@SuppressWarnings("unchecked")
-		List<FileItem> fileItems = fileUpload.parseRequest(request);
-		for (FileItem fileItem : fileItems) {
-			if (!fileItem.isFormField()) {
-				return fileItem;
-			}
-		}
-		return null;
-	}
+  private FileItem getUploadedFile(ServletFileUpload fileUpload, HttpServletRequest request) throws FileUploadException {
+    @SuppressWarnings("unchecked")
+    List<FileItem> fileItems = fileUpload.parseRequest(request);
+    for (FileItem fileItem : fileItems) {
+      if (!fileItem.isFormField()) {
+        return fileItem;
+      }
+    }
+    return null;
+  }
 
 }
--- jodconverter-sample-webapp/src/main/java/org/artofsolving/jodconverter/sample/web/WebappContextListener.java
+++ jodconverter-sample-webapp/src/main/java/org/artofsolving/jodconverter/sample/web/WebappContextListener.java
@@ -1,16 +1,20 @@
 package org.artofsolving.jodconverter.sample.web;
 
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+
 public class WebappContextListener implements ServletContextListener {
 
-	public void contextInitialized(ServletContextEvent event) {
-		WebappContext.init(event.getServletContext());
-	}
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    WebappContext.init(event.getServletContext());
+  }
 
-	public void contextDestroyed(ServletContextEvent event) {
-		WebappContext.destroy(event.getServletContext());
-	}
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    WebappContext.destroy(event.getServletContext());
+  }
 
 }
